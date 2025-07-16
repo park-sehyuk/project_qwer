d57ef96735da261364fbda7961f4fbe82bf00d83
박세혁 <roy1024@naver.com>
2025-07-16 18:30:02 +0900

    헤더 푸터 적용

--- userUI/js/common.js
+++ userUI/js/common.js
@@ -1,5 +1,17 @@
 $(function () {
-  $("#header").load("include/header.html", function () {
+  if (
+    window.location.pathname.includes("/search/") ||
+    window.location.pathname.includes("/userLog/") ||
+    window.location.pathname.includes("/production/")
+  ) {
+    headerPath = "../include/header.html";
+    footerPath = "../include/footer.html";
+  } else {
+    headerPath = "include/header.html";
+    footerPath = "include/footer.html";
+  }
+
+  $("#header").load(headerPath, function () {
     // 헤더가 로드된 후에 실행할 코드
     const searchBtn = document.getElementById("searchBtn");
     const searchResultDiv = document.getElementById("searchResult");
@@ -13,7 +25,7 @@ $(function () {
       }
     });
     const popularSearch = document.querySelectorAll("#srMid ul li button");
-    const searchResultPage = "../search/searchResult.html";
+    const searchResultPage = "search/searchResult.html";
 
     popularSearch.forEach((button) => {
       button.addEventListener("click", function () {
@@ -41,7 +53,7 @@ $(function () {
       if (enteredText) {
         const targetUrl = `${searchResultPage}?query=${encodeURIComponent(
           enteredText
-        )}}`;
+        )}`;
         window.location.href = targetUrl;
       } else {
         alert("검색어를 입력해주세요!");
@@ -51,18 +63,23 @@ $(function () {
       searchInput.value = "";
     });
   });
+
+  $("#footer").load(footerPath, function () {
+    // 푸터가 로드된 후에 실행할 코드
+  });
 });
-const productData = [
-  {
-    imageUrl: "../img/printcottongucci.PNG",
-    name: "프린트 코튼 저지 티셔츠",
-    content: "구찌",
-    price: "830000",
-    category: "반팔",
-    color1: "",
-    color2: "",
-    color3: "",
-    detail:
-      "새로운 시즌을 맞아 진화를 거듭하는 구찌의 하우스 코드와 저지 소재의 만남. 구찌 웹(Web) 배너 프린트가 돋보이는 레귤러 핏 코튼 저지 티셔츠.",
-  },
-];
+
+// const productData = [
+//   {
+//     imageUrl: "../img/printcottongucci.PNG",
+//     name: "프린트 코튼 저지 티셔츠",
+//     content: "구찌",
+//     price: "830000",
+//     category: "반팔",
+//     color1: "",
+//     color2: "",
+//     color3: "",
+//     detail:
+//       "새로운 시즌을 맞아 진화를 거듭하는 구찌의 하우스 코드와 저지 소재의 만남. 구찌 웹(Web) 배너 프린트가 돋보이는 레귤러 핏 코튼 저지 티셔츠.",
+//   },
+// ];
